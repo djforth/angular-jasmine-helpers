211021bc64b0ba71fac4a69da022d87d59a08a05
Adrian Stainforth <adrian@morsedigtial.com>
2015-04-14 16:19:58 +0100

    update

--- dist/date_creator.js
+++ dist/date_creator.js
@@ -26,6 +26,10 @@ DateCreator = (function() {
     this.day_date = this.date.getDate();
   }
 
+  DateCreator.prototype.create_date = function() {
+    return this.dateFmt.formatDate("%Y-%m-%d");
+  };
+
   DateCreator.prototype.create_dateTime = function() {
     return this.dateFmt.formatDate("%Y-%m-%d %H:%M");
   };
@@ -36,50 +40,47 @@ DateCreator = (function() {
     return this.dateFmt.formatDate("%Y-%m-%d %H:%M");
   };
 
-  DateCreator.prototype.create_date = function() {
-    return this.dateFmt.formatDate("%Y-%m-%d");
-  };
-
   DateCreator.prototype.create_date_obj = function(str) {
-    var dateFmt;
-    dateFmt = new DateFormatter(str);
-    return dateFmt.getDate();
+    this.dateFmt = new DateFormatter(str);
+    return this.dateFmt.getDate();
   };
 
-  DateCreator.prototype.create_datetime_obj = function(str) {
-    var dateFmt;
-    dateFmt = new DateFormatter(str);
-    return dateFmt.getDate();
+  DateCreator.prototype.create_time = function() {
+    return this.dateFmt.formatDate("%H:%M");
+  };
+
+  DateCreator.prototype.create_timeLater = function() {
+    this.date.setHours(this.date.getHours() + 1);
+    this.dateFmt = new DateFormatter(this.date);
+    return this.dateFmt.formatDate("%H:%M");
   };
 
   DateCreator.prototype.incrementTime = function() {
-    var date, dateFmt;
-    date = this.dateFmt.getDate();
+    var year;
+    year = this.date.getFullYear();
     this.time++;
     if (this.time === 3) {
       this.date_day++;
       this.time = 0;
     }
-    if (this.day >= this.month_list[this.month - 1]) {
+    if (this.date_day >= this.month_list[this.month]) {
       this.month++;
       this.date_day = 1;
     }
-    dateFmt = new DateFormatter();
-    this.display.time = dateFmt.fixTime(this.time_list[this.time]);
-    this.display.date = dateFmt.fixTime(this.date_day);
-    return this.display.month = dateFmt.fixTime(this.month);
+    this.dateFmt = new DateFormatter(year, this.month, this.date_day, this.time_list[this.time]);
+    return this.date = this.dateFmt.getDate();
   };
 
   DateCreator.prototype.incrementDate = function() {
-    var dateFmt;
-    this.day++;
-    if (this.day >= this.month_list[this.month - 1]) {
+    var year;
+    year = this.date.getFullYear();
+    this.date_day++;
+    if (this.date_day >= this.month_list[this.month]) {
       this.month++;
-      this.day = 1;
+      this.date_day = 1;
     }
-    dateFmt = new DateFormatter();
-    this.display.date = dateFmt.fixTime(this.day);
-    return this.display.month = dateFmt.fixTime(this.month);
+    this.dateFmt = new DateFormatter(year, this.month, this.date_day, this.time_list[this.time]);
+    return this.date = this.dateFmt.getDate();
   };
 
   return DateCreator;
