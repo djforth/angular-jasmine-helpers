eeb8b20eacc020857e780ec078d8a7735b37e260
Adrian Stainforth <adrian@morsedigital.com>
2016-02-01 23:00:46 +0000

    sort index

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-
-
-module.exports = {
-    colorConvert    : require("./lib/colorConvert")
+var colorConvert    : require("./lib/colorConvert")
   , controllerTests : require("./lib/controller_tests")
   , dateCreator     : require("./lib/date_creator")
   , directiveTests  : require("./lib/directives_tests")
   , factoryTests    : require("./lib/factory_tests")
   , statesTests     : require("./lib/states_tests")
+
+module.exports = {
+    colorConvert    : colorConvert
+  , controllerTests : controllerTest
+  , dateCreator     : dateCreator
+  , directiveTests  : directiveTests
+  , factoryTests    : factoryTests
+  , statesTests     : statesTests
 }
