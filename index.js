143342f749c9990b856081ca6a91732453027911
Adrian Stainforth <adrian@morsedigital.com>
2016-02-01 23:07:26 +0000

    sort index

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var colorConvert    : require("./lib/colorConvert")
-  , controllerTests : require("./lib/controller_tests")
-  , dateCreator     : require("./lib/date_creator")
-  , directiveTests  : require("./lib/directives_tests")
-  , factoryTests    : require("./lib/factory_tests")
-  , statesTests     : require("./lib/states_tests")
+var colorConvert    = require("./lib/colorConvert")
+  , controllerTests = require("./lib/controller_tests")
+  , dateCreator     = require("./lib/date_creator")
+  , directiveTests  = require("./lib/directives_tests")
+  , factoryTests    = require("./lib/factory_tests")
+  , statesTests     = require("./lib/states_tests")
 
 module.exports = {
     colorConvert    : colorConvert
